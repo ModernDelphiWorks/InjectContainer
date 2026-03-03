29378054d768a5bbccae0d927f59cdea4fb9e1ad
Isaque Pinheiro <isaquesp@gmail.com>
2026-03-03 19:07:44 -0300

    Commit
    Refatorando

--- Test Delphi/UTesteInject.pas
+++ Test Delphi/UTesteInject.pas
@@ -1,3 +1,16 @@
+{
+  ------------------------------------------------------------------------------
+  InjectContainer
+  Lightweight dependency injection container for Delphi applications.
+
+  SPDX-License-Identifier: Apache-2.0
+  Copyright (c) 2025-2026 Isaque Pinheiro
+
+  Licensed under the Apache License, Version 2.0.
+  See the LICENSE file in the project root for full license information.
+  ------------------------------------------------------------------------------
+}
+
 unit UTesteInject;
 
 interface
